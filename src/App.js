c343c6870eac9074db2c04babd6be75ee4e414a0
TotalOverdoseExists <verylonghair00@gmail.com>
2021-02-04 17:51:34 +0500

    Almost finished auth page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import {
 	BrowserRouter as Router,
 	Switch,
@@ -5,9 +6,171 @@ import {
 	Link,
 	useLocation
 } from 'react-router-dom'
+import InputMask from 'react-input-mask'
 import './App.css'
 
 export default function App() {
+	let [userAuthorized, setUser] = useState('false')
+
+	function Header() {
+		let currentLocation = useLocation()
+	
+		const ProfileLink = () => {
+			if(userAuthorized === 'true') {
+				return (
+					<Link to='/profile/'>Личный кабинет</Link>
+				)
+			}
+			else {
+				return (
+					<Link to='/auth/'>Войти</Link>
+				)
+			}
+		}
+	
+		if(currentLocation.pathname === '/') {
+			return (
+				<header>
+					<div id='l-personalPanel'>
+						<span>Клиентский сервис: <a href='mailto:claim@m-food.ru'>claim@m-food.ru</a></span>
+						<ProfileLink/>
+					</div>
+					<div id='l-menuPanel'>
+						<Link to='/'><img className='m-logoImg' src='/img/logo.svg' alt='логотип компании'/></Link>
+						<nav>
+							<a href='#prices'>Цены</a>
+							<a href='#delivery'>Оплата и доставка</a>
+							<a href='#about'>О нас</a>
+						</nav>
+						<div id='l-headerTimeAndTel'>
+							<span>с 8:00 до 22:00</span>
+							<article>
+								<a className='t-bigTel' href='tel:88007758232'>8 800 775 82 32</a>
+								<a href='#popupCall'>Перезвоните мне</a>
+							</article>
+						</div>
+					</div>
+				</header>
+			)
+		}
+		else {
+			return (
+				<header>
+					<div id='l-menuInnerPanel'>
+						<Link to='/'>&lt; Главная</Link>
+						<Link to='/'><img className='m-logoImg' src='/img/logo.svg' alt='логотип компании'/></Link>
+					</div>
+				</header>
+			)
+		}
+	}
+	
+	function Footer() {
+		return (
+			<footer>
+				<div>
+					<Link to='/'><img className='m-logoImg' src='/img/logo.svg' alt='логотип компании'/></Link>
+					<div id='l-contacts'>
+						<h2 className='t-dark'>Наши контакты:</h2>
+						<span className='t-light'>
+							<a href='tel:88007758232'>8 800 775 82 32</a>
+						</span>
+						<span className='t-dark'>
+							115516, г. Москва, ул. Севанская, д. 52, корп. 1, кв. 57
+						</span>
+						<span className='t-light'>
+							Адрес для корреспонденции:
+						</span>
+						<span className='t-dark'>
+							121471, г. Москва , Рябиновая 26 стр 10 БЦ West Plaza
+						</span>
+					</div>
+					<div id='l-copyright'>
+						<span className='t-dark'>
+							Copyright 2021 LLC
+						</span>
+						<span className='t-light'>
+							<a href='/docs/doc.pdf' target='_blank'>Оферта</a>
+						</span>
+						<span className='t-light'>
+							<a href='/docs/doc.pdf' target='_blank'>Политика конфиденциальности</a>
+						</span>
+					</div>
+				</div>
+				<div>
+					<div id='l-footerSocialLinks'>
+						<a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
+							<img src='/img/instagram.png' alt='instagram'/>
+						</a>
+						<a href='https://vk.com/' target='_blank' rel='noreferrer'>
+							<img src='/img/vk.png' alt='vkontakte'/>
+						</a>
+						<a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>
+							<img src='/img/fb.png' alt='facebook'/>
+						</a>
+					</div>
+					<div id='l-footerInfo'>
+						<span className='t-dark t-uppercase'>
+							ООО «МОЯ ЕДА»<br/>
+							ОГРН: 1197746295636<br/>
+							ИНН: 7724474343<br/>
+							КПП: 772501001
+						</span>
+					</div>
+				</div>
+			</footer>
+		)
+	}
+	
+	function Mainpage() {
+		return (
+			<h1>Mainpage</h1>
+		)
+	}
+	
+	function Auth() {
+		
+
+		return (
+			<main>
+				<section className='m-section'>
+					<h1>Ваш телефон</h1>
+					<form id='authForm'>
+						<InputMask mask='+7 (999) 999-9999' maskChar='_' alwaysShowMask='true'/>
+						<span>
+							Нажимая кнопку «Продолжить», Вы соглашаетесь с условиями <a href='/docs/doc.pdf' target='_blank' rel='noreferrer'>оферты</a> и даете согласие на обработку <a href='/docs/doc.pdf' target='_blank' rel='noreferrer'>персональных данных</a>
+						</span>
+						<button type='submit'>Продолжить</button>
+					</form>
+				</section>
+			</main>
+		)
+	}
+	
+	function Profile() {
+		return (
+			<h1>Profile</h1>
+		)
+	}
+	
+	function Orders() {
+		return (
+			<h1>Orders</h1>
+		)
+	}
+	
+	function Basket() {
+		return (
+			<h1>Basket</h1>
+		)
+	}
+	
+	function Delivery() {
+		return (
+			<h1>Delivery</h1>
+		)
+	}
+
 	return (
 		<Router>
 			<Header/>
@@ -24,135 +187,3 @@ export default function App() {
 	)
 }
 
-function Header() {
-	let currentLocation = useLocation()
-
-	if(currentLocation.pathname === '/') {
-		return (
-			<header>
-				<div id='l-personalPanel'>
-					<span>Клиентский сервис: <a href='mailto:claim@m-food.ru'>claim@m-food.ru</a></span>
-					<Link to='/auth/'>Войти</Link>
-				</div>
-				<div id='l-menuPanel'>
-					<Link to='/'><img className='m-logoImg' src='/img/logo.svg' alt='логотип компании'/></Link>
-					<nav>
-						<a href='#prices'>Цены</a>
-						<a href='#delivery'>Оплата и доставка</a>
-						<a href='#about'>О нас</a>
-					</nav>
-					<div id='l-headerTimeAndTel'>
-						<span>с 8:00 до 22:00</span>
-						<article>
-							<a className='t-bigTel' href='tel:88007758232'>8 800 775 82 32</a>
-							<a href='#popupCall'>Перезвоните мне</a>
-						</article>
-					</div>
-				</div>
-			</header>
-		)
-	}
-	else {
-		return (
-			<header>
-				<div id='l-menuInnerPanel'>
-					<Link to='/'>&lt; Главная</Link>
-					<Link to='/'><img className='m-logoImg' src='/img/logo.svg' alt='логотип компании'/></Link>
-				</div>
-			</header>
-		)
-	}
-}
-
-function Footer() {
-	return (
-		<footer>
-			<div>
-				<Link to='/'><img className='m-logoImg' src='/img/logo.svg' alt='логотип компании'/></Link>
-				<div id='l-contacts'>
-					<h2 className='t-dark'>Наши контакты:</h2>
-					<span className='t-light'>
-						<a href='tel:88007758232'>8 800 775 82 32</a>
-					</span>
-					<span className='t-dark'>
-						115516, г. Москва, ул. Севанская, д. 52, корп. 1, кв. 57
-					</span>
-					<span className='t-light'>
-						Адрес для корреспонденции:
-					</span>
-					<span className='t-dark'>
-						121471, г. Москва , Рябиновая 26 стр 10 БЦ West Plaza
-					</span>
-				</div>
-				<div id='l-copyright'>
-					<span className='t-dark'>
-						Copyright 2021 LLC
-					</span>
-					<span className='t-light'>
-						<a href='/docs/doc.pdf' target='_blank'>Оферта</a>
-					</span>
-					<span className='t-light'>
-						<a href='/docs/doc.pdf' target='_blank'>Политика конфиденциальности</a>
-					</span>
-				</div>
-			</div>
-			<div>
-				<div id='l-footerSocialLinks'>
-					<a href='https://www.instagram.com/' target='_blank'>
-						<img src='/img/instagram.png'/>
-					</a>
-					<a href='https://vk.com/' target='_blank'>
-						<img src='/img/vk.png'/>
-					</a>
-					<a href='https://www.facebook.com/' target='_blank'>
-						<img src='/img/fb.png'/>
-					</a>
-				</div>
-				<div id='l-footerInfo'>
-					<span className='t-dark t-uppercase'>
-						ООО «МОЯ ЕДА»<br/>
-						ОГРН: 1197746295636<br/>
-						ИНН: 7724474343<br/>
-						КПП: 772501001
-					</span>
-				</div>
-			</div>
-		</footer>
-	)
-}
-
-function Mainpage() {
-	return (
-		<h1>Mainpage</h1>
-	)
-}
-
-function Auth() {
-	return (
-		<h1>Authorization</h1>
-	)
-}
-
-function Profile() {
-	return (
-		<h1>Profile</h1>
-	)
-}
-
-function Orders() {
-	return (
-		<h1>Orders</h1>
-	)
-}
-
-function Basket() {
-	return (
-		<h1>Basket</h1>
-	)
-}
-
-function Delivery() {
-	return (
-		<h1>Delivery</h1>
-	)
-}
