58889b2c4890bb6564d7c9c91ebb9221f0919826
TotalOverdoseExists <verylonghair00@gmail.com>
2021-02-11 17:59:23 +0500

    Finished auth and user sessions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
 	Link,
+	NavLink,
 	Redirect,
 	useLocation
 } from 'react-router-dom'
 import InputMask from 'react-input-mask'
 import './App.css'
 
+const apiKey = '3c0d4811f72089fdd67d6ec69ad06889fb03b88a'
+const apiUrl = 'https://api.musi4s.website'
+
 export default function App() {
 	let [userAuthorized, setUser] = useState('false')
+	let userHash = sessionStorage.getItem('userHash')
+	
+	if(userHash) {
+		fetch(apiUrl + '/api/v1/get/auth', {
+			method: 'get',
+			mode: 'cors',
+			headers: {
+				'authorization': apiKey,
+				'user-hash': userHash
+			}
+		})
+		.then(response => {
+			response.json().then(json => {
+				if(json.success === 'true') {
+					if(json.data) {
+						setUser('true')
+					}
+				}
+			})
+		})
+		.catch(error => {
+			console.log('User not authorized.')
+		})
+	}
 
 	function Header() {
 		let currentLocation = useLocation()
@@ -132,14 +160,48 @@ export default function App() {
 	function Auth() {
 		let [telInput, setTelInput] = useState('tel')
 		let [codeInput, setCodeInput] = useState('hidden')
+		let [timerButtonClass, setTimerButtonClass] = useState('s-hidden')
+		let [counter, setCounter] = useState(0)
+		let [continueButton, setContinueButton] = useState('submit')
+		
+		const formatTime = time => {
+			const minutes = Math.floor(time / 60)
+			const seconds = time % 60
+			return `${minutes}:${String(seconds).length === 1 ? `0${seconds}` : `${seconds}`}`
+		}
+		
+		useEffect(() => {
+			let timer
+
+			if(counter > 0) {
+				timer = setTimeout(() => setCounter(c => c - 1), 1000)
+			}
+
+			return () => {
+				if(timer) {
+					clearTimeout(timer)
+				}
+			}
+		}, [counter])
+
+		const codeChanged = e => {
+			const regEx = /\d{4}/
+			if(regEx.test(e.target.value)) {
+				setContinueButton('submit')
+			}
+			else {
+				setContinueButton('button')
+			}
+		}
+
 		const sendTel = e => {
 			e.preventDefault()
 
-			fetch('https://api.musi4s.website/api/v1/post/auth', {
+			fetch(apiUrl + '/api/v1/post/auth', {
 				method: 'post',
 				mode: 'cors',
 				headers: {
-					'authorization': '3c0d4811f72089fdd67d6ec69ad06889fb03b88a'
+					'authorization': apiKey
 				},
 				body: new FormData(e.target)
 			})
@@ -151,52 +213,149 @@ export default function App() {
 							if(json.data.event === 'code') {
 								setTelInput('hidden')
 								setCodeInput('text')
+								setTimerButtonClass('t-timerButton')
+								setCounter(300)
+								setContinueButton('button')
 							}
 							else if(json.data.event === 'auth') {
-								<Redirect to='/profile/'/>
+								sessionStorage.setItem('userHash', json.data.hash)
+								setUser('true')
 							}
 						}
 					})
 				}
+				else {
+					alert('Ошибка сервера! Пожалуйста, попробуйте позже.')
+				}
 			})
 			.catch(error => {
-				console.log(error)
 				alert('Ошибка сервера! Пожалуйста, попробуйте позже.')
 			})
 		}
 		
-		return (
-			<main>
-				<section className='m-section'>
-					<h1>Ваш телефон</h1>
-					<form id='authForm' onSubmit={sendTel}>
-						<InputMask mask='+7 (999) 999-9999' maskChar='_' alwaysShowMask='true'>
-							{(inputProps) => <input type={telInput} name='AuthForm[phone]'/>}
-						</InputMask>
-						<InputMask mask='9999' maskChar='_' alwaysShowMask='true'>
-							{(inputProps) => <input type={codeInput} name='AuthForm[code]'/>}
-						</InputMask>
-						<input type='submit' value='Отправить повторно ()'/>
-						<span>
-							Нажимая кнопку «Продолжить», Вы соглашаетесь с условиями <a href='/docs/doc.pdf' target='_blank' rel='noreferrer'>оферты</a> и даете согласие на обработку <a href='/docs/doc.pdf' target='_blank' rel='noreferrer'>персональных данных</a>
-						</span>
-						<button type='submit'>Продолжить</button>
-					</form>
-				</section>
-			</main>
-		)
+		if(userAuthorized === 'true') {
+			return (
+				<Redirect to='/profile/'/>
+			)
+		}
+		else {
+			return (
+				<main>
+					<section className='m-section'>
+						<h1>Ваш телефон</h1>
+						<form id='l-authForm' onSubmit={sendTel}>
+							<InputMask mask='+7 (999) 999-9999' maskChar='_' alwaysShowMask='true'>
+								{(inputProps) => <input type={telInput} name='AuthForm[phone]'/>}
+							</InputMask>
+							<InputMask mask='9999' maskChar='_' alwaysShowMask='true' onChange={codeChanged}>
+								{(inputProps) => <input type={codeInput} name='AuthForm[code]'/>}
+							</InputMask>
+							<button className={timerButtonClass} type={counter === 0 ? 'submit' : 'button'}>Отправить повторно ({formatTime(counter)})</button>
+							<span>
+								Нажимая кнопку «Продолжить», Вы соглашаетесь с условиями <a href='/docs/doc.pdf' target='_blank' rel='noreferrer'>оферты</a> и даете согласие на обработку <a href='/docs/doc.pdf' target='_blank' rel='noreferrer'>персональных данных</a>
+							</span>
+							<button type={continueButton}>Продолжить</button>
+						</form>
+					</section>
+				</main>
+			)
+		}
 	}
 	
 	function Profile() {
-		return (
-			<h1>Profile</h1>
-		)
+		const logout = () => {
+			fetch(apiUrl + '/api/v1/get/auth?type=exit', {
+				method: 'get',
+				mode: 'cors',
+				headers: {
+					'authorization': apiKey,
+					'user-hash': sessionStorage.getItem('userHash')
+				}
+			})
+			.then(response => {
+				response.json().then(json => {
+					console.log(json)
+					if(json.success === 'true') {
+						sessionStorage.removeItem('userHash')
+						setUser('false')
+					}
+				})
+			})
+			.catch(error => {
+				console.log('User not authorized.')
+			})
+		}
+
+		if(userAuthorized === 'false') {
+			return (
+				<Redirect to='/auth/'/>
+			)
+		}
+		else {
+			return (
+				<main>
+					<section className='m-section'>
+						<div id='l-profileLinks'>
+							<NavLink to='/profile/' activeClassName='active'>Профиль</NavLink>
+							<NavLink to='/orders/' activeClassName='active'>Заказы</NavLink>
+							<a href='#' onClick={logout}>Выйти</a>
+						</div>
+						<div id='l-personalInfo'>
+							<input type='file' name='UserForm[picture]'/>
+							<article>
+								<label>Телефон</label>
+								<span></span>
+							</article>
+						</div>
+					</section>
+				</main>
+			)
+		}
 	}
 	
 	function Orders() {
-		return (
-			<h1>Orders</h1>
-		)
+		const logout = () => {
+			fetch(apiUrl + '/api/v1/get/auth?type=exit', {
+				method: 'get',
+				mode: 'cors',
+				headers: {
+					'authorization': apiKey,
+					'user-hash': sessionStorage.getItem('userHash')
+				}
+			})
+			.then(response => {
+				response.json().then(json => {
+					console.log(json)
+					if(json.success === 'true') {
+						sessionStorage.removeItem('userHash')
+						setUser('false')
+					}
+				})
+			})
+			.catch(error => {
+				console.log('User not authorized.')
+			})
+		}
+
+		if(userAuthorized === 'false') {
+			return (
+				<Redirect to='/auth/'/>
+			)
+		}
+		else {
+			return (
+				<main>
+					<section className='m-section'>
+						<div id='l-profileLinks'>
+							<NavLink to='/profile/' activeClassName='active'>Профиль</NavLink>
+							<NavLink to='/orders/' activeClassName='active'>Заказы</NavLink>
+							<a href='#' onClick={logout}>Выйти</a>
+						</div>
+						<div id='l-personalInfo'></div>
+					</section>
+				</main>
+			)
+		}
 	}
 	
 	function Basket() {
