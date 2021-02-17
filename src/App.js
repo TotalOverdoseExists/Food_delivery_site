67aacfad3006aaf4b40d77f586740b9662be3e9d
TotalOverdoseExists <verylonghair00@gmail.com>
2021-02-17 17:44:22 +0500

    Finished almost finished profile and orders pages, started main page

--- src/App.js
+++ src/App.js
@@ -9,10 +9,24 @@ import {
 	useLocation
 } from 'react-router-dom'
 import InputMask from 'react-input-mask'
+import Modal from 'react-modal'
 import './App.css'
 
 const apiKey = '3c0d4811f72089fdd67d6ec69ad06889fb03b88a'
 const apiUrl = 'https://api.musi4s.website'
+
+const modalStyles = {
+	content: {
+		top: '25%',
+		left: '25%',
+		right: '25%',
+		bottom: '25%',
+		borderRadius: '1rem',
+		padding: '2.5rem 5rem'
+	}
+}
+
+Modal.setAppElement('#root')
 
 export default function App() {
 	let [userAuthorized, setUserAuthorized] = useState(false)
@@ -42,6 +56,7 @@ export default function App() {
 
 	function Header() {
 		let currentLocation = useLocation()
+		const [modalIsOpen, setIsOpen] = useState(false)
 	
 		const ProfileLink = () => {
 			if(userAuthorized) {
@@ -54,6 +69,14 @@ export default function App() {
 					<Link to='/auth/'>Войти</Link>
 				)
 			}
+		}
+
+		const openModal = () => {
+			setIsOpen(true)
+		}
+
+		const closeModal = () => {
+			setIsOpen(false)
 		}
 	
 		if(currentLocation.pathname === '/') {
@@ -74,7 +97,25 @@ export default function App() {
 							<span>с 8:00 до 22:00</span>
 							<article>
 								<a className='t-bigTel' href='tel:88007758232'>8 800 775 82 32</a>
-								<a href='#popupCall'>Перезвоните мне</a>
+								<button className='m-linkButton' onClick={openModal}>Перезвоните мне</button>
+								<Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={modalStyles}>
+									<button className='m-linkButton m-closeModalButton' onClick={closeModal}>&#10006;</button>
+									<h2>Заказать обратный звонок</h2>
+									<form>
+										<article>
+											<input type='text' name='RequestForm[name]' placeholder='Имя'/>
+										</article>
+										<article>
+											<InputMask mask='+7 (999) 999-9999' maskChar='_' alwaysShowMask='true'>
+												{(inputProps) => <input type='tel' name='RequestForm[phone]'/>}
+											</InputMask>
+										</article>
+										<article>
+											<input type='checkbox' name='RequestForm[agreement]' id='agreement'/>
+										</article>
+										<button type='submit'>Отправить</button>
+									</form>
+								</Modal>
 							</article>
 						</div>
 					</div>
@@ -152,7 +193,11 @@ export default function App() {
 	
 	function Mainpage() {
 		return (
-			<h1>Mainpage</h1>
+			<main>
+				<section className='m-section'>
+
+				</section>
+			</main>
 		)
 	}
 	
@@ -274,6 +319,8 @@ export default function App() {
 		const [error, setError] = useState(null)
 		const [isLoaded, setIsLoaded] = useState(false)
 		const [items, setItems] = useState([])
+		const [modalIsOpen, setIsOpen] = useState(false)
+		const [addressChanged, setAddressChanged] = useState(0)
 
 		useEffect(() => {
 			let isMounted = true
@@ -291,6 +338,7 @@ export default function App() {
 				result => {
 					setIsLoaded(true)
 					setItems(result)
+					console.log(result)
 				},
 				error => {
 					setIsLoaded(true)
@@ -325,6 +373,85 @@ export default function App() {
 			})
 		}
 
+		const openModal = () => {
+			setIsOpen(true)
+		}
+
+		const closeModal = () => {
+			setIsOpen(false)
+		}
+
+		const editInfo = e => {
+			e.preventDefault()
+
+			fetch(apiUrl + '/api/v1/post/update?type=user', {
+				method: 'post',
+				mode: 'cors',
+				headers: {
+					'authorization': apiKey,
+					'user-hash': sessionStorage.getItem('userHash')
+				},
+				body: new FormData(e.target)
+			})
+			.then(response => {
+				response.json().then(json => {
+					if(json.success) {
+						alert('Данные успешно обновлены!')
+					}
+				})
+			})
+			.catch(error => {
+				alert('Ошибка сервера! Пожалуйста, попробуйте позже.')
+			})
+		}
+
+		const addAddress = e => {
+			e.preventDefault()
+
+			fetch(apiUrl + '/api/v1/post/address', {
+				method: 'post',
+				mode: 'cors',
+				headers: {
+					'authorization': apiKey,
+					'user-hash': sessionStorage.getItem('userHash')
+				},
+				body: new FormData(e.target)
+			})
+			.then(response => {
+				response.json().then(json => {
+					if(json.success) {
+						alert('Данные успешно обновлены!')
+					}
+				})
+			})
+			.catch(error => {
+				alert('Ошибка сервера! Пожалуйста, попробуйте позже.')
+			})
+		}
+
+		const deleteAddress = e => {
+			fetch(apiUrl + '/api/v1/get/address?id=' + e.target.dataset.id, {
+				method: 'get',
+				mode: 'cors',
+				headers: {
+					'authorization': apiKey,
+					'user-hash': sessionStorage.getItem('userHash')
+				}
+			})
+			.then(response => {
+				response.json().then(json => {
+					if(json.success) {
+						console.log('Jeje!')
+						setAddressChanged(count => count + 1)
+						console.log(addressChanged)
+					}
+				})
+			})
+			.catch(error => {
+				alert('Ошибка сервера! Пожалуйста, попробуйте позже.')
+			})
+		}
+
 		if(!userAuthorized) {
 			return (
 				<Redirect to='/auth/'/>
@@ -343,7 +470,7 @@ export default function App() {
 						<section className='m-section'>
 							<ProfileLinks/>
 							<div id='l-personal'>
-								<form id='l-personalInfo'>
+								<form id='l-personalInfo' onSubmit={editInfo}>
 									<h2>+79224188882</h2>
 									<article>
 										<input type='text' name='UserForm[name]' placeholder='Имя' defaultValue='Сергей'/>
@@ -372,10 +499,39 @@ export default function App() {
 								</form>
 								<div id='l-personalAddresses'>
 									<h3>Мои адреса</h3>
-									<a href='#'>Добавить адрес</a>
+									<button className='m-linkButton' onClick={openModal}>Добавить адрес</button>
+									<Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={modalStyles}>
+										<button className='m-linkButton m-closeModalButton' onClick={closeModal}>&#10006;</button>
+										<h2>Добавить новый адрес</h2>
+										<form id='l-modalAddressForm' onSubmit={addAddress}>
+											<article>
+												<input type='text' name='AddressForm[name]' placeholder='Название'/>
+											</article>
+											<article>
+												<input type='text' name='AddressForm[address]' placeholder='Адрес'/>
+											</article>
+											<article>
+												<input type='text' name='AddressForm[driveway]' placeholder='Подъезд'/>
+											</article>
+											<article>
+												<input type='text' name='AddressForm[apartment]' placeholder='Квартира'/>
+											</article>
+											<article>
+												<input type='text' name='AddressForm[intercom]' placeholder='Домофон'/>
+											</article>
+											<article>
+												<input type='text' name='AddressForm[story]' placeholder='Этаж'/>
+											</article>
+											<button type='submit'>Сохранить</button>
+										</form>
+									</Modal>
 									<article>
-										<p>Москва, 12-я Парковая улица, 6 12 </p>
-										<a href='#'>Удалить</a>
+										<p><span>Дом: </span>Москва, 12-я Парковая улица, 6 12</p>
+										<button className='m-linkButton' data-id='1' onClick={deleteAddress}>Удалить</button>
+									</article>
+									<article>
+										<p><span>Работа: </span>Москва, 12-я Парковая улица, 6 12</p>
+										<button className='m-linkButton' data-id='2' onClick={deleteAddress}>Удалить</button>
 									</article>
 								</div>
 							</div>
@@ -387,6 +543,25 @@ export default function App() {
 	}
 	
 	function Orders() {
+		fetch(apiUrl + '/api/v1/get/orders?type=list', {
+			method: 'get',
+			mode: 'cors',
+			headers: {
+				'authorization': apiKey,
+				'user-hash': sessionStorage.getItem('userHash')
+			}
+		})
+		.then(response => {
+			response.json().then(json => {
+				if(json.success) {
+					console.log(json)
+				}
+			})
+		})
+		.catch(error => {
+			alert('Ошибка сервера! Пожалуйста, попробуйте позже.')
+		})
+
 		if(!userAuthorized) {
 			return (
 				<Redirect to='/auth/'/>
@@ -397,7 +572,63 @@ export default function App() {
 				<main>
 					<section className='m-section'>
 						<ProfileLinks/>
-						<div id='l-personalInfo'></div>
+						<div id='l-personal'>
+							<div id='l-personalOrders'>
+								<h2>Список заказов пуст</h2>
+								<article>
+									<div>
+										<span>Номер заказа</span>
+										<p>1</p>
+									</div>
+									<div>
+										<span>Дата заказа</span>
+										<p>17.02.2021, 13:33:08</p>
+									</div>
+									<div>
+										<span>Количество дней</span>
+										<p>30 дней</p>
+									</div>
+									<div>
+										<span>Сумма заказа</span>
+										<p>20 000 руб</p>
+									</div>
+									<div>
+										<span>Способ оплаты</span>
+										<p>Картой курьеру</p>
+									</div>
+									<div>
+										<span>Статус заказа</span>
+										<p>Не оплачен</p>
+									</div>
+								</article>
+								<article>
+									<div>
+										<span>Номер заказа</span>
+										<p>1</p>
+									</div>
+									<div>
+										<span>Дата заказа</span>
+										<p>17.02.2021, 13:33:08</p>
+									</div>
+									<div>
+										<span>Количество дней</span>
+										<p>30 дней</p>
+									</div>
+									<div>
+										<span>Сумма заказа</span>
+										<p>20 000 руб</p>
+									</div>
+									<div>
+										<span>Способ оплаты</span>
+										<p>Картой курьеру</p>
+									</div>
+									<div>
+										<span>Статус заказа</span>
+										<p>Не оплачен</p>
+									</div>
+								</article>
+							</div>
+						</div>
 					</section>
 				</main>
 			)
@@ -407,12 +638,6 @@ export default function App() {
 	function Basket() {
 		return (
 			<h1>Basket</h1>
-		)
-	}
-	
-	function Delivery() {
-		return (
-			<h1>Delivery</h1>
 		)
 	}
 
@@ -431,9 +656,6 @@ export default function App() {
 				</Route>
 				<Route path='/basket/'>
 					<Basket/>
-				</Route>
-				<Route path='/delivery/'>
-					<Delivery/>
 				</Route>
 				<Route path='/'>
 					<Mainpage/>
